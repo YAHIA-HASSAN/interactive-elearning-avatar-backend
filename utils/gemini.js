2ae10d6ebdaef6a53e08495d7005171225810cfc
Yahia Hassan <77647892+YAHIA-HASSAN@users.noreply.github.com>
2024-05-03 16:26:47 +0300

    gemini update message format

--- utils/gemini.js
+++ utils/gemini.js
@@ -7,17 +7,25 @@ module.exports = async function genAIPrompet(prompet) {
   // For dialog language tasks (like chat), use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  const chat = model.startChat();
-
-  const msg = `you arer a virual teacher avatar and a student asks you: '${prompet}'.
-    you have the following actions to peform ["clap", waving", "nod", "moving hands", "ideal"]
-    you always reply in json format as following:
-    {
-        "response":"",
-        "action":"",
-    }`;
-
-  const result = await chat.sendMessage(msg);
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: ` You are a virtual teacher. You are always replay with a JSON format as follows: {
+          "responses": ["",""],
+           "actions":["",""] 
+          }. With minimum 3 and maximum 6 strings elements. Each of them is an array of Strings. The different actions are: 'claping', 'waving', 'nodding', 'moving_hands', and 'ideal'. With different actions. ` }],
+      },
+    ],
+    generationConfig: {
+      maxOutputTokens: 4000,
+    },
+  });
+  const result = await chat.sendMessage(prompet);
   const response = result.response;
   const text = response.text();
   return JSON.parse(text);
