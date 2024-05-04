29b37d23c416a4477ef5cbb516e42de5030d8092
Yahia Hassan <77647892+YAHIA-HASSAN@users.noreply.github.com>
2024-05-04 18:46:19 +0300

    login edit format to json

--- routes/auth.js
+++ routes/auth.js
@@ -5,15 +5,14 @@ const UserModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-router.post("/", (req, res) => {
-  if(!validator(req, res)) return;
+router.post("/", validator,async (req, res) => {
   // Check email
-  let user = UserModel.findOne({ "U_Email": req.body.U_Email }).exec();
+  let user = await UserModel.findOne({ "U_Email": req.body.U_Email }).exec();
   if (!user) return res.status(400).send({"status":"Invalid email"});
 
   // Check password
-  const validPassword = bcrypt.compare(req.body.U_Password, user.U_Password);
-  if (!validPassword) return res.status(400).send("Invalid password");
+  const validPassword = await bcrypt.compare(req.body.U_Password, user.U_Password);
+  if (!validPassword) return res.status(400).send({"status":"Invalid password"});
   
   const token = jwt.sign({ U_ID: user._id }, process.env.TOKEN_SECRET);
   res.header("x-auth-token", token);
